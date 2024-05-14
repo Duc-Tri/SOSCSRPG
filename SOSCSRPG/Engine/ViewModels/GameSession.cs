bc872fe0fbe1f27dc08103b80dea2df55d9fd5b7
Tri <ductri.vu@gmail.com>
2024-05-14 23:19:19 +0200

    Lesson 03.5 - Displaying the Player Object

--- SOSCSRPG/Engine/ViewModels/GameSession.cs
+++ SOSCSRPG/Engine/ViewModels/GameSession.cs
@@ -9,13 +9,17 @@ namespace Engine.ViewModels
 {
     public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()
         {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Scott";
             CurrentPlayer.Gold = 1_000_000;
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints= 10;
+            CurrentPlayer.ExperiencePoints= 0;
+            CurrentPlayer.Level = 1;
         }
     }
 }
