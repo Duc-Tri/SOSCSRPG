15e1ee61acd0dcbc4b7087ee1c462c5a01fe7e2e
Tri <ductri.vu@gmail.com>
2024-05-24 23:58:45 +0200

    Lesson 07.1_ Creating Monsters

---
+++ SOSCSRPG/Engine/RandomNumberGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public static class RandomNumberGenerator
+    {
+        public static int RandomNumber(int minimumValue, int maximumValue)
+        {
+            return 0;
+        }
+    }
+}
