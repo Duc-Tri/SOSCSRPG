3b0127ba34075e865beee3e2cacb036bed720a6d
Tri <ductri.vu@gmail.com>
2024-06-17 00:41:57 +0200

    bug : trade screen doesn't show

--- SOSCSRPG/WPFUI/MainWindow.xaml.cs
+++ SOSCSRPG/WPFUI/MainWindow.xaml.cs
@@ -53,6 +53,14 @@ namespace WPFUI
             GameMessages.ScrollToEnd();
         }
 
+        private void OnClick_DisplayTradeScreen(object sender, RoutedEventArgs e)
+        {
+            TradeScreen tradeScreen = new TradeScreen();
+            tradeScreen.Owner = this;
+            tradeScreen.DataContext = _gameSession;
+            tradeScreen.ShowDialog();
+        }
+
     }
 
 }
